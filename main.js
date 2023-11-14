a1b403c200dee825d9fc6f0d52aa7bcf6f703b95
Pamela Yura <pamela_yura@hotmail.com>
2023-11-14 18:51:52 -0300

    iniciando commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form-agenda');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+
+    let linha = '<tr>';
+    linha += `<td> ${inputNomeContato.value}</td>`;
+    linha += `<td> ${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoAgenda = document.querySelector('tbody');
+    corpoAgenda.innerHTML = linhas;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+});
